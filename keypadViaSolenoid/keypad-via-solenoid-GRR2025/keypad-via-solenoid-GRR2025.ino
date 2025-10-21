947d46fa335d399b70d609bcf4c33df7681a0459
Benjamin <ben.lac5@icloud.com>
2025-10-21 18:08:34 -0400

    Update keypad-via-solenoid-GRR2025.ino

--- keypadViaSolenoid/keypad-via-solenoid-GRR2025/keypad-via-solenoid-GRR2025.ino
+++ keypadViaSolenoid/keypad-via-solenoid-GRR2025/keypad-via-solenoid-GRR2025.ino
@@ -6,10 +6,12 @@ Version: October 7th, 2025
 
 // Defined Pins:
 
-int solenoid7 = 3; // PINS CURRENTLY UNDEFINED!
 int solenoid3 = 2;
+int solenoid7 = 3;
 int solenoid8 = 4;
 int solenoidPound = 5;
+int delayAfterHigh= 100;
+int delayAfterLow = 2000;
 
 void setup() {
   // Pins set as output:
@@ -22,37 +24,40 @@ void setup() {
   // Solenoid sequence runs:
   // Code: 73738# (RESET#)
 
-  digitalWrite(solenoid7, HIGH); // Solenoid presses 7.
-  delay(1000);
-  digitalWrite(solenoid7, LOW);
-  delay(1000);
-
-  digitalWrite(solenoid3, HIGH); // Solenoid presses 3.
-  delay(1000);
-  digitalWrite(solenoid3, LOW);
-  delay(1000);
-
-  digitalWrite(solenoid7, HIGH); // Solenoid presses 7.
-  delay(1000);
-  digitalWrite(solenoid7, LOW);
-  delay(1000);
-
-  digitalWrite(solenoid3, HIGH); // Solenoid presses 3.
-  delay(1000);
-  digitalWrite(solenoid3, LOW);
-  delay(1000);
-
-  digitalWrite(solenoid8, HIGH); // Solenoid presses 8.
-  delay(1000);
-  digitalWrite(solenoid8, LOW);
-  delay(1000);
-
-  digitalWrite(solenoidPound, HIGH); // Solenoid presses #.
-  delay(1000);
-  digitalWrite(solenoidPound, LOW);
-  delay(1000);
+  
 }
 
 void loop() {
   // Code only needs to run once.
+  digitalWrite(solenoid7, HIGH); // Solenoid presses 7.
+  delay(delayAfterHigh);
+  digitalWrite(solenoid7, LOW);
+  delay(delayAfterLow);
+
+  digitalWrite(solenoid3, HIGH); // Solenoid presses 3.
+  delay(delayAfterHigh);
+  digitalWrite(solenoid3, LOW);
+  delay(delayAfterLow);
+
+  digitalWrite(solenoid7, HIGH); // Solenoid presses 7.
+  delay(delayAfterHigh);
+  digitalWrite(solenoid7, LOW);
+  delay(delayAfterLow);
+
+  digitalWrite(solenoid3, HIGH); // Solenoid presses 3.
+  delay(delayAfterHigh);
+  digitalWrite(solenoid3, LOW);
+  delay(delayAfterLow);
+
+  digitalWrite(solenoid8, HIGH); // Solenoid presses 8.
+  delay(delayAfterHigh);
+  digitalWrite(solenoid8, LOW);
+  delay(delayAfterLow);
+
+  digitalWrite(solenoidPound, HIGH); // Solenoid presses #.
+  delay(delayAfterHigh);
+  digitalWrite(solenoidPound, LOW);
+  delay(delayAfterLow);
+
+  delay(5000);
 }
