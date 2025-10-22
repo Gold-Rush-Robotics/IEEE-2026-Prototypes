8d52e25459971d1d5507e3526ac78310a33f2e54
YutaAI <136610150+YutaAI@users.noreply.github.com>
2025-10-21 20:00:04 -0400

    Optimized Solenoid Code
    Reduced a lot of clutter in repeating code and shortened to an array of iteration.

--- keypadViaSolenoid/keypad-via-solenoid-GRR2025/keypad-via-solenoid-GRR2025.ino
+++ keypadViaSolenoid/keypad-via-solenoid-GRR2025/keypad-via-solenoid-GRR2025.ino
@@ -6,12 +6,13 @@ Version: October 7th, 2025
 
 // Defined Pins:
 
-int solenoid3 = 2;
-int solenoid7 = 3;
+int solenoid3 = 3;
+int solenoid7 = 2;
 int solenoid8 = 4;
 int solenoidPound = 5;
 int delayAfterHigh= 100;
 int delayAfterLow = 2000;
+int soleArr[6] = {solenoid7, solenoid3, solenoid7, solenoid3, solenoid8, solenoidPound};
 
 void setup() {
   // Pins set as output:
@@ -27,37 +28,20 @@ void setup() {
   
 }
 
+void funcInputter(int num)
+{
+  digitalWrite(num, HIGH);
+  delay(delayAfterHigh);
+  digitalWrite(num, LOW);
+  delay(delayAfterLow);
+}
+
 void loop() {
-  // Code only needs to run once.
-  digitalWrite(solenoid7, HIGH); // Solenoid presses 7.
-  delay(delayAfterHigh);
-  digitalWrite(solenoid7, LOW);
-  delay(delayAfterLow);
-
-  digitalWrite(solenoid3, HIGH); // Solenoid presses 3.
-  delay(delayAfterHigh);
-  digitalWrite(solenoid3, LOW);
-  delay(delayAfterLow);
-
-  digitalWrite(solenoid7, HIGH); // Solenoid presses 7.
-  delay(delayAfterHigh);
-  digitalWrite(solenoid7, LOW);
-  delay(delayAfterLow);
-
-  digitalWrite(solenoid3, HIGH); // Solenoid presses 3.
-  delay(delayAfterHigh);
-  digitalWrite(solenoid3, LOW);
-  delay(delayAfterLow);
-
-  digitalWrite(solenoid8, HIGH); // Solenoid presses 8.
-  delay(delayAfterHigh);
-  digitalWrite(solenoid8, LOW);
-  delay(delayAfterLow);
-
-  digitalWrite(solenoidPound, HIGH); // Solenoid presses #.
-  delay(delayAfterHigh);
-  digitalWrite(solenoidPound, LOW);
-  delay(delayAfterLow);
+  // Prototype Optimization 2
+  for (int i = 0; i<6; i++)
+  {
+    funcInputter(soleArr[i]);
+  }
 
   delay(5000);
 }
